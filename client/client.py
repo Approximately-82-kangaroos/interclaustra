c8f35d3410ca84c94b3182cf26ca19e5fa634ec8
Evan Skyberg <evnskg@proton.me>
2023-09-13 21:07:34 -0600

    Initial commit

---
+++ client/client.py
@@ -0,0 +1,7 @@
+# TODO:
+# Add client creation
+# Add file queries/reception
+# Add problem processing
+# Add multicore support
+# Add solution sending
+# https://www.youtube.com/watch?v=JkWdNrmbNEQ
